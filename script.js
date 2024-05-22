bf62de2c6644bf0b50a3d77e77f72d180d80ce00
Kirubakaran98 <kirubakaranthirumeni@gmail.com>
2024-05-22 13:41:37 +0530

    final

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menu=document.getElementsByClassName("nav-menu");
 const openMenu=document.getElementsByClassName("fa-bars");
 const closeMenu=document.getElementsByClassName("fa-x");
+const closeMenu_1=document.querySelectorAll(".nav-menu");
 const overlay=document.getElementsByClassName("data-overlay");
 
 
@@ -8,12 +9,19 @@ const overlay=document.getElementsByClassName("data-overlay");
 for(let i=0;i<closeMenu.length;i++){
     closeMenu[i].addEventListener("click",()=>{
         menu[i].classList.remove('active');
-        overlay[i].classList.remove('active')
+        overlay[i].classList.remove('active');
     })
 }
 for (let i = 0; i <openMenu.length; i++) {
     openMenu[i].addEventListener("click",()=>{
       menu[i].classList.add('active');
-      overlay[i].classList.add('active')
+      overlay[i].classList.add('active');
     })
-  }
+}
+
+for (let i = 0; i <closeMenu_1.length; i++) {
+  closeMenu_1[i].addEventListener("click",()=>{
+    menu[i].classList.remove('active');
+    overlay[i].classList.remove('active');
+  })
+}
